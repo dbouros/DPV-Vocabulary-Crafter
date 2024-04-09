d9e11e02fc9c8c1beee960138fb4430dc0ee2de7
dbouros <jimmisp92@gmail.com>
2024-04-09 21:42:49 +0300

    Initialized @RestController Class

--- src/main/java/com/DPV_Vocabulary_Crafter/Server/VocabularyManager_Controller.java
+++ src/main/java/com/DPV_Vocabulary_Crafter/Server/VocabularyManager_Controller.java
@@ -1,4 +1,9 @@
 package com.DPV_Vocabulary_Crafter.Server;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class VocabularyManager_Controller {
 }
