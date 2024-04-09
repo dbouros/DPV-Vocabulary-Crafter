e5f2fed6db663ea676a464a808d437bff1672019
dbouros <jimmisp92@gmail.com>
2024-04-09 19:55:07 +0300

    New actual project created!

---
+++ src/main/java/com/DPV_Vocabulary_Crafter/DpvVocabularyCrafterApplication.java
@@ -0,0 +1,13 @@
+package com.DPV_Vocabulary_Crafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpvVocabularyCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpvVocabularyCrafterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DPV_Vocabulary_Crafter/DpvVocabularyCrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DPV_Vocabulary_Crafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpvVocabularyCrafterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
