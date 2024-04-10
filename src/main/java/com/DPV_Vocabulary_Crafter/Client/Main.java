d88d7161e74bbef4ce01176ab29af86b5fc81d23
dbouros <jimmisp92@gmail.com>
2024-04-10 23:33:13 +0300

    'Client' and 'Server' are up to date! Refactoring Completed!

--- src/main/java/com/DPV_Vocabulary_Crafter/Client/Main.java
+++ src/main/java/com/DPV_Vocabulary_Crafter/Client/Main.java
@@ -3,5 +3,33 @@ package com.DPV_Vocabulary_Crafter.Client;
 public class Main {
     public static void main(String[] args) {
 
+
+        // Client's DAO object!
+        Web_DAO_Client web_dao_clnt = new Web_DAO_Client();
+
+        // Get a "Hello" response from "Server".
+        System.out.println(web_dao_clnt.get_hello("http://localhost:8080/api/hello"));
+
+        // View original DPV from "Server".
+        System.out.println("Original DPV:");
+        System.out.println(web_dao_clnt.get_view_DPV("http://localhost:8080/api/view_DPV/0"));
+
+        String folder_Path = "C:\\Users\\lenovo\\Desktop";
+        String filename = "dpv.rdf";
+
+        // Upload our personal DPV to "Server".
+        web_dao_clnt.get_upload_DPV_RDFFile("http://localhost:8080/api/upload_DPV_RDFFile", folder_Path, filename);
+
+        // View our uploaded personal DPV from Server.
+        System.out.println("Personal DPV:");
+        System.out.println(web_dao_clnt.get_view_DPV("http://localhost:8080/api/view_DPV/1"));
+
+        // Download our personal DPV from "Server".
+			/*
+			 No matter the specified "filename" from the "Server" we as a "Client" can specify our own
+			 preferred name for the file when we download it.
+			*/
+        web_dao_clnt.get_download_DPV_RDFFile("http://localhost:8080/api/download_DPV_RDFFile", folder_Path, filename);
+
     }
 }
--- src/main/java/com/DPV_Vocabulary_Crafter/Client/Web_DAO_Client.java
+++ src/main/java/com/DPV_Vocabulary_Crafter/Client/Web_DAO_Client.java
@@ -1,4 +1,77 @@
 package com.DPV_Vocabulary_Crafter.Client;
 
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
 public class Web_DAO_Client {
+
+    private final RestTemplate restTemplate;
+    private final Local_DAO_Client lcl_dao_clnt;
+
+    public Web_DAO_Client(){
+        this.restTemplate = new RestTemplate();
+        this.lcl_dao_clnt = new Local_DAO_Client();
+    }
+
+    public String get_view_DPV(String Url){
+
+        ResponseEntity<String> response = restTemplate.getForEntity(Url, String.class);
+        String model_String = response.getBody();
+
+        if (model_String != null) {
+            return model_String;
+        }else {
+            System.out.println("String representation of model is null!");
+            return null;
+        }
+
+    }
+
+    public void get_download_DPV_RDFFile(String Url, String folder_Path, String filename){
+
+        ResponseEntity<byte[]> response = restTemplate.getForEntity(Url, byte[].class);
+
+        if (response.getStatusCode().is2xxSuccessful())
+            lcl_dao_clnt.write_VocabularyToFile(response.getBody(), folder_Path, filename);
+        else
+            System.out.println("Failed to download file! Status code: " + response.getStatusCode());
+    }
+
+    public void get_upload_DPV_RDFFile(String Url, String folder_Path, String filename){
+
+        try {
+
+            byte[] fileBytes = lcl_dao_clnt.read_VocabularyFromFile(folder_Path, filename);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_XML);
+
+            // Set the byte array as the request body and the content type header
+            HttpEntity<byte[]> request = new HttpEntity<>(fileBytes, headers);
+
+            // POST request
+            ResponseEntity<byte[]> response = restTemplate.exchange(Url, HttpMethod.POST, request, byte[].class);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                // byte[] responseBody = response.getBody();
+                System.out.println("File uploaded successfully!");
+            } else {
+                throw new Exception("File failed to upload! Status code: " + response.getStatusCode());
+            }
+
+        }catch (Exception e){
+            System.out.println("Upload Failed!, Exception occurred!");
+        }
+    }
+
+    public String get_hello(String Url){
+
+        ResponseEntity<String> response = restTemplate.getForEntity(Url, String.class);
+        if (response.getStatusCode().is2xxSuccessful()){
+            return response.getBody();
+        }else{
+            return "Internal Server Error! Status code: " + response.getStatusCode();
+        }
+    }
+
 }
