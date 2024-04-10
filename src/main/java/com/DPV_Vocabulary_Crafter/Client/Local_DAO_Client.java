0532e45bbfd905e91265a59cd74c3df596c902cd
dbouros <jimmisp92@gmail.com>
2024-04-10 21:04:56 +0300

    Created 'Local_DAO_Client' class!

--- src/main/java/com/DPV_Vocabulary_Crafter/Client/Local_DAO_Client.java
+++ src/main/java/com/DPV_Vocabulary_Crafter/Client/Local_DAO_Client.java
@@ -1,4 +1,53 @@
 package com.DPV_Vocabulary_Crafter.Client;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Local_DAO_Client {
+
+    public void write_VocabularyToFile(byte[] fileBytes, String folder_Path, String filename){
+
+        Path filePath = Paths.get(folder_Path, filename);
+
+        /*
+         1) This try-catch will automatically close the OutputStream at the end of the block.
+         2) The .toFile() method converts the Path object represented by filePath to a File object. In other
+         words, it gets the File representation of the path.
+        */
+        try (OutputStream outputStream = new FileOutputStream(filePath.toFile())) {
+            if (fileBytes != null) {
+                /*
+                 This method writes the content of the fileBytes array to the file specified by filePath.
+                 If the file does not exist, it will be created. If the file already exists, its content
+                 will be overwritten with the new data.
+                */
+                outputStream.write(fileBytes);
+            }else throw new IOException();
+        }catch (IOException e){
+            System.out.println("File Not Found! IOException!");
+        }
+
+        System.out.println("File downloaded successfully! Download Path: " + filePath);
+    }
+
+    public byte[] read_VocabularyFromFile(String folder_Path, String filename){
+
+        Path filePath = Paths.get(folder_Path, filename);
+
+        try {
+            System.out.println("Read bytes from RDF file successfully!");
+            // Read the file's content from the "Path" variable and return it as "byte[]"(ByteArray).
+            return Files.readAllBytes(filePath);
+        } catch (IOException e) {
+            System.out.println("IOException occurred when reading vocabulary from file!");
+            System.out.println(" File is either empty or does not exist! File could not be loaded!");
+            // Returns empty ByteArray "byte[]".
+            return new byte[0];
+        }
+    }
+
 }
--- src/main/java/com/DPV_Vocabulary_Crafter/Server/DpvVocabularyCrafterApplication.java
+++ src/main/java/com/DPV_Vocabulary_Crafter/Server/DpvVocabularyCrafterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DpvVocabularyCrafterApplication {
 
 	public static void main(String[] args) {
-		// The Server Application runs here!!
+		// The Spring Boot Application Server runs here!!
 		SpringApplication.run(DpvVocabularyCrafterApplication.class, args);
 	}
 
