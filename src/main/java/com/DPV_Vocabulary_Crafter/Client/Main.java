b8f4a576809bb5921c73f1bdcc88e4d1b471c21e
dbouros <jimmisp92@gmail.com>
2024-04-25 17:26:53 +0300

    Refactoring content!

--- src/main/java/com/DPV_Vocabulary_Crafter/Client/Main.java
+++ src/main/java/com/DPV_Vocabulary_Crafter/Client/Main.java
@@ -15,10 +15,10 @@ public class Main {
         System.out.println(web_dao_clnt.getViewDPV("http://localhost:8080/api/viewDPV/0"));
 
 // Upload, View, Download Personal DPV from "Server".
-        String folder_Path = "C:\\Users\\lenovo\\Desktop";
+        String folderPath = "C:\\Users\\lenovo\\Desktop";
         String filename = "dpv.rdf";
         // Upload personal DPV to "Server".
-        //web_dao_clnt.postUploadDPVrdfFile("http://localhost:8080/api/uploadDPVrdfFile", folder_Path, filename);
+        //web_dao_clnt.postUploadDPVrdfFile("http://localhost:8080/api/uploadDPVrdfFile", folderPath, filename);
 
         // View uploaded personal DPV from "Server".
         //System.out.println("Personal DPV:");
@@ -30,31 +30,53 @@ public class Main {
 			 preferred name for the file when we download it. Keeping the name of an already existing
 			 model will overwrite that model with the one we will download.
 			*/
-        //web_dao_clnt.getDownloadDPVrdfFile("http://localhost:8080/api/downloadDPVrdfFile", folder_Path, filename);
+        //web_dao_clnt.getDownloadDPVrdfFile("http://localhost:8080/api/downloadDPVrdfFile", folderPath, filename);
 
 // Create, Add term, View, Remove term, Download new DPV from "Server".
         String filename2 = "new_dpv.rdf";
 
         // Create new personal DPV in "Server".
-        web_dao_clnt.getCreateNewDPV("http://localhost:8080/api/createNewDPV");
+        //web_dao_clnt.getCreateNewDPV("http://localhost:8080/api/createNewDPV");
 
         // View new personal DPV from "Server".
-        System.out.println("Personal DPV:");
-        System.out.println(web_dao_clnt.getViewDPV("http://localhost:8080/api/viewDPV/1"));
+        //System.out.println("Personal DPV:");
+        //System.out.println(web_dao_clnt.getViewDPV("http://localhost:8080/api/viewDPV/1"));
 
         // Download new personal DPV from "Server".
-        web_dao_clnt.getDownloadDPVrdfFile("http://localhost:8080/api/downloadDPVrdfFile", folder_Path, filename2);
+        //web_dao_clnt.getDownloadDPVrdfFile("http://localhost:8080/api/downloadDPVrdfFile", folderPath, filename2);
     }
 }
 
 
-// UI Panel checking code !!
-/*UIPanel UIPanel = new UIPanel();
-        UIPanel.run();
-
+// UI Panel checking code. (Method: "main()") !!
+/*
+    UIPanel uiPanel = new UIPanel();
         Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("Press any non-zero number if you want to stay or zero if you want to exit the program!");
+        int command;
+        blankLines15();
+        while (true){
+            System.out.println("Press '>1' to keep looping.");
+            System.out.println("Press '1' to launch panel.");
+            System.out.println("Press '0' to exit the program.");
             System.out.print("Command: ");
-        }while (scanner.nextInt() != 0);
+
+            command = scanner.nextInt();
+
+            if ( command == 0) break;
+            if (command == 1) uiPanel.run();
+            blankLines15();
+        }
+
+        scanner.nextLine(); // Clear the scanner's buffer!
+        System.out.print("Please give a Folder path: ");
+        String folder_Path = scanner.nextLine();
+        System.out.println("Folder Path: " + folder_Path);
+
+        System.out.println("Bye bye!");
+
+    }
+
+    public static void blankLines15(){
+        for (int i=0; i<15; i++) System.out.println();
+    }
 */
