1b1dfccccbdcb9a355d82794f75db39e46707ae7
dbouros <jimmisp92@gmail.com>
2024-05-29 20:32:50 +0300

    Commented code! Method: 'UIPanel'.

--- src/main/java/com/DPV_Vocabulary_Crafter/Client/UIPanel.java
+++ src/main/java/com/DPV_Vocabulary_Crafter/Client/UIPanel.java
@@ -10,35 +10,37 @@ public class UIPanel {
         Font monospacedFont = new Font(Font.MONOSPACED, Font.PLAIN, 22);
 
         JTextArea textArea = new JTextArea();
-        textAreaConfig(textArea, text, monospacedFont);
+        textAreaConfigurations(textArea, text, monospacedFont);
 
         JScrollPane scrollPane = new JScrollPane(textArea);
 
         JFrame frame = new JFrame();
-        frameConfig(frame, scrollPane, title);
+        frameConfigurations(frame, scrollPane, title);
 
     }
 
-    public void textAreaConfig(JTextArea textArea, String text, Font font){
-        textArea.setText(text);
-        textArea.setFont(font);
-        textArea.setEditable(false); // Make the text area read-only
+    public void textAreaConfigurations(JTextArea textArea, String text, Font font){
+        textArea.setText(text); // Set 'Text'.
+        textArea.setFont(font); // Set 'Font'.
+        textArea.setEditable(false); // Make the text area read-only.
         textArea.setBackground(new Color(0x1e1f22)); // Set text's background color to black (Color.BLACK) or intellij IDEA's color: "0x1e1f22" (new Color(0x1e1f22)).
         textArea.setForeground(new Color(0xbcbec4)); // Set text's color to white (Color.WHITE) or intellij IDEA's color: "0x1e1f22" (new Color(0xbcbec4)).
 
     }
 
-    public void frameConfig(JFrame frame, JScrollPane scrollPane, String title){
-        frame.getContentPane().add(scrollPane);
+    public void frameConfigurations(JFrame frame, JScrollPane scrollPane, String title){
+        frame.getContentPane().add(scrollPane); // Adding the 'Scroll Panel' to the 'Frame'.
+        // Set 'Background'.
         frame.getContentPane().setBackground(new Color(0x1e1f22)); // intellij IDEA's color: "0x1e1f22" (new Color(0x1e1f22)).
-        frame.setTitle(title);
-        frame.setSize(1366, 545);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setVisible(true);
+        frame.setTitle(title); // Set 'Title'.
+        frame.setSize(1366, 545); // Set 'Frame's Size'.
+        frame.setLocationRelativeTo(null); // Set 'Frame's Position' to center.
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Dispose 'Frame' on close.
+        frame.setVisible(true); // Set the 'Frame' to visible.
     }
 
     public void run(String title, String text) {
+        // Using lambda expression.
         SwingUtilities.invokeLater(() -> { createAndShowGUI(title, text); });
     }
 
